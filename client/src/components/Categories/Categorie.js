bd2bf2dab799beb0c3eba9c31b199b348390acfb
Iva Tosheva <otevi55@gmail.com>
2021-03-21 15:13:43 +0200

    added categories nav component

--- client/src/components/Categories/Categorie.js
+++ client/src/components/Categories/Categorie.js
@@ -1,22 +1,8 @@
-import { Button } from 'react-bootstrap';
-import './Categories.css'
-import { BsHouseDoorFill, BsFillHouseFill, BsFillPuzzleFill } from 'react-icons/bs'
-import { AiFillCar } from 'react-icons/ai';
-import { GiFlowerPot, GiClothes } from 'react-icons/gi';
-import {TiSortAlphabetically} from 'react-icons/ti';
+import CategoriesNav from "./CategoriesNav";
 
 function Categories() {
     return (
-        <div className="container" id="categories">
-            <h1>Categories</h1>
-            <Button variant="dark" id="all"><TiSortAlphabetically />All</Button>{' '}
-            <Button variant="dark" id="properties"><BsHouseDoorFill />Properties</Button>{' '}
-            <Button variant="dark" id="auto"><AiFillCar />Auto</Button>{' '}
-            <Button variant="dark" id="home"><BsFillHouseFill />Home</Button>{' '}
-            <Button variant="dark" id="clothes"><GiClothes />Clothes</Button>{' '}
-            <Button variant="dark" id="toys"><BsFillPuzzleFill />Toys</Button>{' '}
-            <Button variant="dark" id="garden"><GiFlowerPot />Garden</Button>{' '}
-        </div>
+        <CategoriesNav />
     )
 }
 
---
+++ client/src/components/Categories/CategoriesNav.js
@@ -0,0 +1,38 @@
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Categories.css'
+import { BsHouseDoorFill, BsFillHouseFill, BsFillPuzzleFill } from 'react-icons/bs'
+import { AiFillCar } from 'react-icons/ai';
+import { GiFlowerPot, GiClothes } from 'react-icons/gi';
+import { TiSortAlphabetically } from 'react-icons/ti';
+
+function CategoriesNav() {
+    return (
+        <div className="container" id="categories">
+            <h1>Categories</h1>
+            <Link to="/categories/all">
+                <Button variant="dark" id="all"><TiSortAlphabetically />All</Button>{' '}
+            </Link>
+            <Link to="/categories/properties">
+                <Button variant="dark" id="properties"><BsHouseDoorFill />Properties</Button>{' '}
+            </Link>
+            <Link to="/categories/auto">
+                <Button variant="dark" id="auto"><AiFillCar />Auto</Button>{' '}
+            </Link>
+            <Link to="/categories/home">
+                <Button variant="dark" id="home"><BsFillHouseFill />Home</Button>{' '}
+            </Link>
+            <Link to="/categories/clothes">
+                <Button variant="dark" id="clothes"><GiClothes />Clothes</Button>{' '}
+            </Link>
+            <Link to="/categories/toys">
+                <Button variant="dark" id="toys"><BsFillPuzzleFill />Toys</Button>{' '}
+            </Link>
+            <Link to="/categories/garden">
+                <Button variant="dark" id="garden"><GiFlowerPot />Garden</Button>{' '}
+            </Link>
+        </div>
+    )
+}
+
+export default CategoriesNav;
