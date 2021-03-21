022d1cd718d49b396b2cc5d807730aa8a8de239e
Iva Tosheva <otevi55@gmail.com>
2021-03-21 15:06:52 +0200

    added categories buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Image } from 'react-bootstrap';
 
-
 import Header from './components/Header/Header';
-import TopProducts from './components/Products/TopProducts'
+import TopProducts from './components/Products/TopProducts';
+import Categories from './components/Categories/Categorie';
+
 function App() {
    return (
       <>
          <Header />
          <Image src="http://store.picbg.net/pubpic/46/C5/4241166cd1c646c5.jpg" fluid />
+         <Categories />
          <TopProducts />
       </>
    );
