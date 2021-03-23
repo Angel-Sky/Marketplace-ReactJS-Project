680d10649a8da028838634a5acd09cd81005757d
Iva Tosheva <otevi55@gmail.com>
2021-03-23 23:05:52 +0200

    register page styled

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -11,7 +11,7 @@ function Login() {
                 <Form className="col-lg-6">
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" required/>
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                     </Form.Text>
@@ -19,7 +19,7 @@ function Login() {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" required />
                     </Form.Group>
                     <Button className="col-lg-12 btnAuth" variant="dark" type="submit">Sign in</Button>
                 </Form>
--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,28 +1,30 @@
 import { Form, Button } from 'react-bootstrap'
+import SimpleSider from '../../Siders/SimpleSider';
 import '../auth.css';
 
 function Register() {
     return (
-        <div className="container auth-form">
-            <h1 className="auth-heading">Sign Up</h1>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Reepeat Password</Form.Label>
-                    <Form.Control type="repeatPassword" placeholder="Repeat password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Sign Up
-                </Button>
-            </Form>
-        </div>
+        <>
+            <SimpleSider />
+            <div className="container auth-form">
+                <h1 className="auth-heading">Sign Up</h1>
+                <Form className="col-lg-6">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" required />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Reepeat Password</Form.Label>
+                        <Form.Control type="repeatPassword" placeholder="Repeat password" required />
+                    </Form.Group>
+                    <Button variant="dark" className="col-lg-12 btnAuth" type="submit">Sign Up</Button>
+                </Form>
+            </div>
+        </>
     )
 }
 
