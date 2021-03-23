475e337c86c248b2d35bc750fc1f6c63684ae39a
Iva Tosheva <otevi55@gmail.com>
2021-03-23 19:23:26 +0200

    Added contact seller button at details page

--- client/src/components/Product/Aside.js
+++ client/src/components/Product/Aside.js
@@ -1,14 +1,40 @@
-import { Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { RiMessage3Fill } from 'react-icons/ri';
+
 
 function Aside({ params }) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <aside>
-            <div id="priceLabel">
+            <div id="priceLabel" className="col-lg-12">
                 <h4>Product Price </h4>
                 <h1>{params.price} €</h1>
             </div>
-            <Button variant="dark">Contact Seller</Button>{' '}
+            <Button variant="dark" className="col-lg-10" id="btnContact" onClick={handleShow}>
+                <RiMessage3Fill />Contact Seller
+            </Button>{' '}
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Message</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="dark" onClick={handleClose}>Sent</Button>
+                </Modal.Footer>
+            </Modal>
         </aside>
     )
 }
