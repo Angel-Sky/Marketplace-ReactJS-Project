10f0e3f38547c94dcb42941ef7359acc18114e29
Iva Tosheva <otevi55@gmail.com>
2021-03-23 16:43:55 +0200

    Sider component added with background-image and search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { Image } from 'react-bootstrap';
 
 import Header from './components/Header/Header';
+import Sider from './components/Sider/Sider'
 import Categories from './components/Categories/Categorie';
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
 import Error404 from './components/Error404/Error404';
 
+
 function App() {
    return (
       <>
          <Header />
-         <Image src="http://store.picbg.net/pubpic/46/C5/4241166cd1c646c5.jpg" fluid />
+         <Sider />
          <Switch>
             <Route path="/" exact component={Categories} />
             <Route path="/categories/:category" component={Categories} />
-            <Route path="/auth/login" exact component={Login}/>
-            <Route path="/auth/register" exact component={Register}/>
+            <Route path="/auth/login" exact component={Login} />
+            <Route path="/auth/register" exact component={Register} />
             <Route component={Error404} />
          </Switch>
       </>
---
+++ client/src/components/Sider/Sider.js
@@ -0,0 +1,11 @@
+import './Sider.css'
+
+function Sider() {
+    return (
+        <div id="sider">
+            <input className="col-lg-6" type="text" placeholder="Search..." ></input>
+        </div>
+    )
+}
+
+export default Sider;
