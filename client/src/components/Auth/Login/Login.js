e3370d2c15bf5ba923353412bf8cc16e85f6c8a5
Iva Tosheva <otevi55@gmail.com>
2021-03-23 22:59:13 +0200

    login page styled

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -1,28 +1,30 @@
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap';
+import SimpleSider from '../../Siders/SimpleSider';
 import '../auth.css';
 
 function Login() {
     return (
-        <div className="container auth-form">
-            <h1 className="auth-heading">Sign In</h1>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+        <>
+            <SimpleSider />
+            <div className="container auth-form">
+                <h1 className="auth-heading">Sign In</h1>
+                <Form className="col-lg-6">
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
                     </Form.Text>
-                </Form.Group>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Sign in
-                </Button>
-            </Form>
-        </div>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button className="col-lg-12 btnAuth" variant="dark" type="submit">Sign in</Button>
+                </Form>
+            </div>
+        </>
     )
 }
 
